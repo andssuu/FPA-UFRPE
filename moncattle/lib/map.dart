73f76d3d8cc3127762e915e24696a06100bdcd68
Anderson <anderson.silvasantos@ufrpe.br>
2020-09-15 08:44:39 -0300

    add marcadores no mapa

--- moncattle/lib/map.dart
+++ moncattle/lib/map.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'models/cow_notifier.dart';
 
 class CowMap extends StatefulWidget {
   @override
@@ -8,8 +10,10 @@ class CowMap extends StatefulWidget {
 }
 
 class _CowMapState extends State<CowMap> {
+  //MapType _currentMapType = MapType.normal;
   Completer<GoogleMapController> _controller = Completer();
   static const LatLng _center = const LatLng(-8.014995, -34.949509);
+
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
@@ -22,15 +26,50 @@ class _CowMapState extends State<CowMap> {
         elevation: .1,
         backgroundColor: Colors.black87,
       ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
-          child: GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 15.0,
-            ),
-          )),
+      body: _buildMap(),
     );
+  }
+
+  Widget _buildMap() {
+    return Consumer<CowNotifier>(
+      builder: (context, animals, child) {
+        return Container(
+            padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+            child: GoogleMap(
+              markers: getMarkers(),
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 15.0,
+              ),
+            ));
+      },
+    );
+  }
+
+  Set<Marker> getMarkers() {
+    Set<Marker> _markers = {};
+    var animals = context.read<CowNotifier>();
+    animals.cows.forEach(
+      (cow) {
+        print(cow.name);
+        print(cow.breed);
+        print(cow.lat);
+        print(cow.lon);
+        _markers.add(
+          Marker(
+            // This marker id can be anything that uniquely identifies each marker.
+            markerId: MarkerId(LatLng(cow.lat, cow.lon).toString()),
+            position: LatLng(cow.lat, cow.lon),
+            infoWindow: InfoWindow(
+              title: cow.name,
+              snippet: cow.idCollar,
+            ),
+            icon: BitmapDescriptor.defaultMarker,
+          ),
+        );
+      },
+    );
+    return _markers;
   }
 }
