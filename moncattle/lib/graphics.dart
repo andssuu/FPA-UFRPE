1adbfe49d00351f3a53d154d1d68b78ed90f8d58
Anderson <anderson.silvasantos@ufrpe.br>
2020-09-21 14:48:03 -0300

    add grafico do historico de cadastro de animais

--- moncattle/lib/graphics.dart
+++ moncattle/lib/graphics.dart
@@ -23,13 +23,69 @@ class _GraphicsState extends State<Graphics> {
           crossAxisCount: 1,
           padding: EdgeInsets.all(3.0),
           children: <Widget>[
+            _buildChartHist(),
             _buildChartsBreed(),
-            //_buildChartsBreed(),
             //_buildChartsBreed(),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildChartHist() {
+    return Consumer<CowNotifier>(
+      builder: (context, animals, child) {
+        //print(animals.cows);
+        return Container(
+          height: 300,
+          padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+          child: new charts.TimeSeriesChart(
+            _getHistAnimals(),
+            animate: true,
+            defaultRenderer: new charts.LineRendererConfig(includePoints: true),
+            dateTimeFactory: const charts.LocalDateTimeFactory(),
+            behaviors: [
+              new charts.ChartTitle('Histórico de animais cadastrados',
+                  behaviorPosition: charts.BehaviorPosition.top,
+                  titleOutsideJustification: charts.OutsideJustification.start,
+                  innerPadding: 30),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  List<charts.Series<LinearTimeSeriesCows, DateTime>> _getHistAnimals() {
+    var animals = context.read<CowNotifier>();
+    int total = animals.cows.length;
+    print(total);
+    var map = Map();
+    animals.cows.forEach(
+      (cow) {
+        if (!map.containsKey(cow.hist)) {
+          map[cow.hist] = 1;
+        } else {
+          map[cow.hist] += 1;
+        }
+      },
+    );
+    print(map);
+    List<LinearTimeSeriesCows> data = [];
+    map.forEach((key, value) {
+      data.add(new LinearTimeSeriesCows(key, value));
+    });
+    return [
+      new charts.Series<LinearTimeSeriesCows, DateTime>(
+        id: 'Breeds',
+        domainFn: (LinearTimeSeriesCows count, _) => count.time,
+        measureFn: (LinearTimeSeriesCows count, _) => count.count,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearTimeSeriesCows row, _) => '${row.count}',
+        colorFn: (_, __) => charts.MaterialPalette.black,
+      )
+    ];
   }
 
   Widget _buildChartsBreed() {
@@ -40,12 +96,12 @@ class _GraphicsState extends State<Graphics> {
           height: 300,
           padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
           child: new charts.BarChart(
-            _createSampleData(),
+            _getBreeds(),
             animate: true,
             barRendererDecorator: new charts.BarLabelDecorator<String>(),
             domainAxis: new charts.OrdinalAxisSpec(),
             behaviors: [
-              new charts.ChartTitle('Raças',
+              new charts.ChartTitle('Quantidade de animals por raças',
                   behaviorPosition: charts.BehaviorPosition.top,
                   titleOutsideJustification: charts.OutsideJustification.start,
                   innerPadding: 20),
@@ -56,10 +112,10 @@ class _GraphicsState extends State<Graphics> {
     );
   }
 
-  List<charts.Series<LinearBreeds, String>> _createSampleData() {
+  List<charts.Series<LinearBreeds, String>> _getBreeds() {
     var animals = context.read<CowNotifier>();
-    int total = animals.cows.length;
-    print(total);
+    //int total = animals.cows.length;
+    //print(total);
     var map = Map();
     animals.cows.forEach(
       (cow) {
@@ -70,7 +126,7 @@ class _GraphicsState extends State<Graphics> {
         }
       },
     );
-    print(map);
+    //print(map);
     List<LinearBreeds> data = [];
     map.forEach((key, value) {
       data.add(new LinearBreeds(key, value));
@@ -93,4 +149,10 @@ class LinearBreeds {
   final String name;
   final int count;
   LinearBreeds(this.name, this.count);
+}
+
+class LinearTimeSeriesCows {
+  final DateTime time;
+  final int count;
+  LinearTimeSeriesCows(this.time, this.count);
 }
